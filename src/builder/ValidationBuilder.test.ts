467c878ad580e8fa5e1d1943d9fc261c53d42d62
nbyx <28953458+nbyx@users.noreply.github.com>
2023-09-25 17:48:56 +0200

    Add initial-builder.test.ts

--- src/builder/ValidationBuilder.test.ts
+++ src/builder/ValidationBuilder.test.ts
@@ -9,8 +9,8 @@ test("ValidationBuilder.create returns an InitialBuilder instance", () => {
 
 test("ValidationBuilder.create respects failFast parameter", () => {
 	const failFastBuilder = ValidationBuilder.create<number>(true);
-	expect((failFastBuilder as any).failFast).toBe(true);
+	expect((failFastBuilder as any).sharedState.failFast).toBe(true);
 
 	const nonFailFastBuilder = ValidationBuilder.create<number>(false);
-	expect((nonFailFastBuilder as any).failFast).toBe(false);
+	expect((nonFailFastBuilder as any).sharedState.failFast).toBe(false);
 });
